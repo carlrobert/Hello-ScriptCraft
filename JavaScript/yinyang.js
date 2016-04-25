b6df063f7dfc213b8759b3b1b8824ce333e8fce8
Robert Storlind <carlrobert@users.noreply.github.com>
2016-04-25 22:08:48 +0200

    material spelling

--- JavaScript/yinyang.js
+++ JavaScript/yinyang.js
@@ -1,4 +1,4 @@
-function halvcirkel(matrial, radie, hojd, left)
+function halvcirkel(material, radie, hojd, left)
 {
   var arcParams = {
     radius: radie,
@@ -7,7 +7,7 @@ function halvcirkel(matrial, radie, hojd, left)
     stack: hojd,
     quadrants: left ? { topleft: true, bottomleft: true } : { topright: true, bottomright: true }
   };
-  var md = this.getBlockIdAndMeta(matrial);
+  var md = this.getBlockIdAndMeta(material);
   arcParams.blockType = md[0];
   arcParams.meta = md[1];
 
@@ -19,28 +19,28 @@ function halvcirkel(matrial, radie, hojd, left)
 }
 Drone.extend('halvcirkel', halvcirkel);
 
-function cirkel(matrial, radie, hojd){ 
+function cirkel(material, radie, hojd){ 
   return this
     .left(radie)
-    .cylinder(matrial, radie, hojd)
+    .cylinder(material, radie, hojd)
     .right(radie)
     ;
 }
 Drone.extend('cirkel', cirkel);
 
-function yinyang(matrial1, matrial2, radie, hojd)
+function yinyang(material1, material2, radie, hojd)
 {
   return this
-    .halvcirkel(matrial1, radie, hojd, true)
-    .halvcirkel(matrial2, radie, hojd, false)
-    .cirkel(matrial2, radie / 2, hojd)
+    .halvcirkel(material1, radie, hojd, true)
+    .halvcirkel(material2, radie, hojd, false)
+    .cirkel(material2, radie / 2, hojd)
     .fwd(radie)
-    .cirkel(matrial1, radie / 2, hojd)
+    .cirkel(material1, radie / 2, hojd)
     .back(radie)
     .fwd(radie / 2)
-    .box(matrial1, 1, hojd, 1)
+    .box(material1, 1, hojd, 1)
     .fwd(radie )
-    .box(matrial2, 1, hojd, 1)
+    .box(material2, 1, hojd, 1)
     .back(radie * 3 / 4)
     ;
 }
